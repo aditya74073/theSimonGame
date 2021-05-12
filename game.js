85894ba171c1b5e09517192d8548d4951088cfb0
Aditya Anal <77203643+aditya74073@users.noreply.github.com>
2021-05-12 22:54:59 +0530

    Update game.js

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keyup(function() {
+$(document).touch(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
